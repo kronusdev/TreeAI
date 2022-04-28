69f834dee4e9824fd81b5e0e3e68672b4bb36ac1
kronusdev <kronusdev@protonmail.com>
2022-04-28 16:47:03 -0500

    asdkaskas

--- treeai.py
+++ treeai.py
@@ -36,11 +36,11 @@ if __name__ == "__main__":
             ),
             interactions.Option(
                 name="max_length",
-                description="What is the maximum word length of your response? (10-150)",
+                description="What is the maximum word length of your response? (10-200)",
                 type=interactions.OptionType.INTEGER,
                 required=True,
                 min_value=10,
-                max_value=150
+                max_value=200
             )
         ]
     )
