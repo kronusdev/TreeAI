17d79ba8e2e1fc078cb66f1f0d6ca4476a520f8e
kronusdev <kronusdev@protonmail.com>
2022-04-28 15:25:46 -0500

    more updates

--- treeai.py
+++ treeai.py
@@ -1,9 +1,29 @@
+import os
 from transformers import pipeline
+from dotenv import load_dotenv
+import discord
+import interactions
 
-generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
+def getGenerator():
+    return pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
 
-prompt = "What is my favorite color?"
+def getResponse(generator, prompt, length, temp):
+    response = generator(prompt, max_length=length, do_sample=True, temperature=temp)
+    return response[0]['generated_text']
 
-res = generator(prompt, max_length=100, do_sample=True, temperature=0.9)
+if __name__ == "__main__":
 
-print(res[0]['generated_text'])
+    # get the .env variables loaded
+    load_dotenv()
+    # get the Discord stuff sorted
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    client = discord.Client()
+
+    # get that generator
+    generator = getGenerator()
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+
+    client.run(TOKEN)
